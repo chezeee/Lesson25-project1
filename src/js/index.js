21c9e1d93968d4a2a318bd80e3119d2f630ee63b
Ilya Yalymov <chezeee@gmail.com>
2023-03-12 17:19:32 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
-import { getWeather } from "./weather.js";
+import { getWeather, result } from "./weather.js";
 
 const citiesDropdown = document.querySelector("#cities-dropdown");
+const cityName = document.querySelector("#city");
 
 citiesDropdown.addEventListener("change", () => {
-  if (citiesDropdown.value) {
+  if (citiesDropdown.value && citiesDropdown.value !== "City") {
+    const city =
+      citiesDropdown.value[0].toUpperCase() +
+      citiesDropdown.value.slice(1, citiesDropdown.value.length);
+
+    cityName.textContent = city;
     getWeather(citiesDropdown.value);
+  } else {
+    result.innerHTML = "&nbsp;";
+    cityName.textContent = "...";
   }
 });
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,9 +1,16 @@
+import FetchWrapper from "./fetch-wrapper.js";
+
 export const getWeather = (city) => {
-  // your code
+  const API = new FetchWrapper(
+    "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app"
+  );
+
+  API.get(`/weatherstack/${city}.json`).then((data) => {
+    showTemperature(data.current.temperature);
+  });
 };
 
-// Do NOT modify the code below
-const result = document.querySelector("#result");
+export const result = document.querySelector("#result");
 const showTemperature = (temperature) => {
   result.textContent = `It's ${temperature} degrees celsius.`;
 };
